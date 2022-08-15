3fcaded624026125b65e2c4639f5dad314ac6eae
Maciej Sroka <sroka.maciej93@gmail.com>
2022-08-15 16:40:02 +0200

    Test for chrome macbook

--- src/ui/components/dragdrop.js
+++ src/ui/components/dragdrop.js
@@ -277,6 +277,7 @@ const dragdrop = (() => {
   };
 
   function handleDrop(e) {
+    console.log(Array.prototype.slice.call(e.dataTransfer.types));
     e.stopPropagation();
     this.classList.remove('over');
     const x = parseInt(this.dataset.x, 10);
